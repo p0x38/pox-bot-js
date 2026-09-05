c3b65696b35a5c97be9a7406b87cf583396b007c
p0x38 <ryuuking0410@gmail.com>
2026-09-06 03:01:32 +0900

    test: configure ESLint for test sources

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ export default tseslint.config([
     {
         languageOptions: {
             parserOptions: {
-                project: true,
+                project: ['./tsconfig.json', './tsconfig.test.json'],
                 tsconfigRootDir: import.meta.dirname,
             },
         },
