681d39805302ff3e0c8b3a7111770626b6e42906
p0x38 <ryuuking0410@gmail.com>
2026-09-08 15:21:52 +0900

    fix: normalize command decorator barrel [skip ci]

---
+++ src/commands/decorators/index.ts
@@ -0,0 +1,13 @@
+export { autocomplete } from './autocomplete';
+export { command } from './command';
+export { execute } from './execute';
+export {
+    getCommandMetadata,
+    getCommandMethodMetadata,
+    isCommandConstructor,
+} from './metadata';
+export type {
+    CommandConstructor,
+    CommandMetadata,
+    CommandMethodMetadata,
+} from './metadata';
