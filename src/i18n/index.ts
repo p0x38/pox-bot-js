09cd3d7d41967e569f8edbff12bdd7689f312141
p0x38 <ryuuking0410@gmail.com>
2026-09-09 23:50:38 +0900

    refactor: remove static config import from i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,4 @@
-import config from '../config.json';
+import { DEFAULT_LANGUAGE } from '@/config/defaults';
 import { getLanguages, getReferenceLang, getNamespaces } from './discovery';
 import { initI18n } from './init';
 import { getLocalePath } from './paths';
@@ -8,7 +8,7 @@ const localesPath = getLocalePath();
 const languages = getLanguages(localesPath);
 console.log('Detected languages:', languages);
 
-const referenceLang = getReferenceLang(languages, config.defaultLanguage);
+const referenceLang = getReferenceLang(languages, DEFAULT_LANGUAGE);
 
 const namespaces = getNamespaces(localesPath, referenceLang);
 
@@ -16,7 +16,7 @@ const i18n = initI18n({
     localesPath,
     languages,
     namespaces,
-    defaultLang: config.defaultLanguage ?? 'en',
+    defaultLang: DEFAULT_LANGUAGE,
 });
 
 export { languages };
