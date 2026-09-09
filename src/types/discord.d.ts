3618779a28d17943a136d9d80cb6f1ab7e7448b3
p0x38 <ryuuking0410@gmail.com>
2026-09-09 23:42:19 +0900

    fix: qualify discord client collection type

--- src/types/discord.d.ts
+++ src/types/discord.d.ts
@@ -2,7 +2,10 @@ import 'discord.js';
 
 declare module 'discord.js' {
     interface Client {
-        commands: Collection<string, import('@/commands/types').Command>;
+        commands: import('discord.js').Collection<
+            string,
+            import('@/commands/types').Command
+        >;
         config: import('@/config').Config;
         configManager: import('@/config').ConfigManager;
         i18n: typeof import('@/i18n').default;
