77272dca04db8952ea601f85c3bd48e656d3ebaf
p0x38 <ryuuking0410@gmail.com>
2026-09-09 23:41:44 +0900

    fix: complete discord client augmentation

--- src/types/discord.d.ts
+++ src/types/discord.d.ts
@@ -1,16 +1,11 @@
-import type { Client } from 'discord.js';
-
-import type { Config } from '@/config';
-import type { ContextServicesMetadata } from '@/contexts/ContextServicesMetadata';
-
-export {};
+import 'discord.js';
 
 declare module 'discord.js' {
     interface Client {
         commands: Collection<string, import('@/commands/types').Command>;
-        config: Config;
+        config: import('@/config').Config;
         configManager: import('@/config').ConfigManager;
         i18n: typeof import('@/i18n').default;
-        services: ContextServicesMetadata;
+        services: import('@/contexts/ContextServicesMetadata').ContextServicesMetadata;
     }
 }
